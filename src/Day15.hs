32ebdbf2c4a207c75d68af97760fce348e2921ac
Peter Gerrard <peter.gerrard@red-gate.com>
2022-12-15 12:49:04 +0000

    Part B

--- src/Day15.hs
+++ src/Day15.hs
@@ -15,8 +15,8 @@ parseSensor inp = Sensor (ox, oy) (sx, sy)
 parse :: String -> [Sensor]
 parse = map parseSensor . lines
 
-getBlockedOnRow :: Integer -> Sensor -> Maybe (Integer, Integer)
-getBlockedOnRow y (Sensor (ox,oy) (sx,sy)) = if l' <= r' then Just (l',r') else Nothing -- [x | x <-[(ox-o)..(ox + o)], x /= sx || y /= sy]
+getBlockedOnRow :: Bool -> Integer -> Sensor -> Maybe (Integer, Integer)
+getBlockedOnRow includeSignal y (Sensor (ox,oy) (sx,sy)) = if l' <= r' then Just (if includeSignal then (l,r) else (l',r')) else Nothing
     where
         md = abs (sx - ox) + abs (sy - oy)
         d = abs (y - oy)
@@ -26,20 +26,21 @@ getBlockedOnRow y (Sensor (ox,oy) (sx,sy)) = if l' <= r' then Just (l',r') else 
         r' = if r == sx then r - 1 else r
 
 mergeRanges :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
-mergeRanges (a,b) (x,y)
-    | a >= x && a <= y = (x, max b y)
-    | x >= a && x <= b = (a, max b y)
-    | otherwise = error "unmergeable"
+mergeRanges (a,b) (x,y) = (min a x, max b y)
 
 addRange :: [(Integer, Integer)] -> (Integer, Integer) -> [(Integer, Integer)]
 addRange [] r = [r]
 addRange (r@(a,b):rs) r1@(x,y)
-    | b < x = r:addRange rs r1
-    | y < a = r1:r:rs
+    | b + 1 < x = r:addRange rs r1
+    | y + 1 < a = r1:r:rs
     | otherwise = addRange rs (mergeRanges r r1)
 
 partA :: [Sensor] -> Integer
-partA = sum . map ((+1) . uncurry (flip (-))) . foldl addRange [] . mapMaybe (getBlockedOnRow 2000000)
+partA = sum . map ((+1) . uncurry (flip (-))) . foldl addRange [] . mapMaybe (getBlockedOnRow False 2000000)
 
 toPos :: (Integer, [(Integer,Integer)]) -> [(Integer, Integer)]
 toPos (y, xs) = map (\x -> (snd x + 1, y)) xs
+
+partB ss = (\(x,y) -> x*4000000 + y) . head . filter inBounds . concatMap toPos . filter ((>1) . length . snd) $ map (\y -> (y, foldl addRange [] $ mapMaybe (getBlockedOnRow True y) ss)) [0..4000000]
+    where
+        inBounds (x,y) = x >= 0 && x <= 4000000 && y >= 0 && y <= 4000000
